ef18e7a254a64835006d09d7793bb6aec00a864b
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-16 10:27:39 +0300

    push error fix

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
