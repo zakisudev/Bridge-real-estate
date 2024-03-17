6a4474945c06b57f83464fc5e1bcfd8b33315d3f
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-17 22:23:09 +0300

    Update Sequelize configuration

--- backend/utilities/database/sequelize.ts
+++ backend/utilities/database/sequelize.ts
@@ -1,27 +1,30 @@
-import config from "config";
-import { Sequelize, Transaction } from "sequelize";
+import config from 'config';
+import { Sequelize, Transaction } from 'sequelize';
 
 export let sequelize: Sequelize;
 
 export default async () => {
-  let dbHost: string = config.get("database.host");
-  let dbName: string = config.get("database.name");
-  let dbUser: string = config.get("database.user");
-  let dbPassword: string = process.env.DB_PASSWORD || "";
+  let dbHost: string = config.get('database.host');
+  let dbName: string = config.get('database.name');
+  let dbUser: string = config.get('database.user');
+  let dbPort: string = config.get('database.port');
+  let dbPassword: string =
+    process.env.DB_PASSWORD || config.get('database.password');
 
   sequelize = new Sequelize(dbName, dbUser, dbPassword, {
     host: dbHost,
-    dialect: "mysql",
+    port: parseInt(dbPort),
+    dialect: 'mysql',
     dialectOptions: { decimalNumbers: true },
     logging: console.log,
   });
   sequelize
     .sync({ force: false, alter: false })
     .then((sequelize) => {
-      console.log("Database connected");
+      console.log('Database connected');
     })
     .catch((err) => {
-      console.log("Database connection failed", err);
+      console.log('Database connection failed', err);
     });
 };
 
