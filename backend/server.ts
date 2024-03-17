f46304f02492329c3b013ec88612d9c198560824
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-03-17 03:33:52 -0700

    Add database configuration and user model

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
-require('dotenv').config();
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+require("dotenv").config();
+import express, { Request, Response } from "express";
+import cors from "cors";
+import initializeDB from "./utilities/database/sequelize";
 
 const port = process.env.PORT || 5000;
 
@@ -11,9 +12,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+initializeDB();
+
 // Routes
-app.use('/api', (req: Request, res: Response) => {
-  res.send('API is running ...');
+app.use("/api", (req: Request, res: Response) => {
+  res.send("API is running ...");
 });
 
 // Start server
---
+++ backend/utilities/database/sequelize.ts
@@ -0,0 +1,37 @@
+import config from "config";
+import { Sequelize, Transaction } from "sequelize";
+
+export let sequelize: Sequelize;
+
+export default async () => {
+  let dbHost: string = config.get("database.host");
+  let dbName: string = config.get("database.name");
+  let dbUser: string = config.get("database.user");
+  let dbPassword: string = process.env.DB_PASSWORD || "";
+
+  sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: "mysql",
+    dialectOptions: { decimalNumbers: true },
+    logging: console.log,
+  });
+  sequelize
+    .sync({ force: false, alter: false })
+    .then((sequelize) => {
+      console.log("Database connected");
+    })
+    .catch((err) => {
+      console.log("Database connection failed", err);
+    });
+};
+
+export const createTransaction = (): Promise<Transaction> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const transaction = await sequelize.transaction();
+      resolve(transaction);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
