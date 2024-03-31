b8fa72fe2a793d0795eb63d45e22106bcc8b3e31
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-03-30 21:32:53 -0700

    Add  Documentation Property and User models

--- backend/models/Property.ts
+++ backend/models/Property.ts
@@ -20,77 +20,135 @@ export class Property extends Model {
   public readonly updatedAt!: Date;
 }
 
+/**
+ * Represents a Property model.
+ */
 export default (sequelize: Sequelize) => {
+  /**
+   * Represents the Property class.
+   */
   Property.init(
     {
+      /**
+       * The unique identifier of the property.
+       */
       id: {
         type: DataTypes.INTEGER.UNSIGNED,
         autoIncrement: true,
         primaryKey: true,
       },
+      /**
+       * The title of the property.
+       */
       title: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
+      /**
+       * The description of the property.
+       */
       description: {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      /**
+       * The address of the property.
+       */
       address: {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      /**
+       * The number of bathrooms in the property.
+       */
       bathrooms: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
+      /**
+       * The number of bedrooms in the property.
+       */
       bedrooms: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
+      /**
+       * Indicates whether the property is furnished or not.
+       */
       furnished: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
       },
+      /**
+       * Indicates whether parking is available for the property or not.
+       */
       parking: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
       },
+      /**
+       * The size of the property in square feet.
+       */
       size: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
+      /**
+       * The discounted price of the property.
+       */
       discountedPrice: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
+      /**
+       * The regular price of the property.
+       */
       regularPrice: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
+      /**
+       * The type of the property.
+       */
       type: {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      /**
+       * Indicates whether there is an offer available for the property or not.
+       */
       offer: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
       },
+      /**
+       * The URLs of the property images.
+       */
       imageUrls: {
         type: DataTypes.TEXT,
         allowNull: false,
+        /**
+         * Getter function to parse the imageUrls from JSON string to an array.
+         */
         get() {
           return this.getDataValue("imageUrls")
             ? JSON.parse(this.getDataValue("imageUrls"))
             : [];
         },
+        /**
+         * Setter function to convert the imageUrls array to a JSON string.
+         * @param value - The imageUrls array.
+         */
         set(value: any) {
           if (value) {
             this.setDataValue("imageUrls", JSON.stringify(value));
           }
         },
       },
+      /**
+       * The unique identifier of the user who owns the property.
+       */
       user_id: {
         type: DataTypes.INTEGER.UNSIGNED,
         allowNull: false,
--- backend/models/User.ts
+++ backend/models/User.ts
@@ -14,39 +14,69 @@ export class User extends Model {
   public readonly updatedAt!: Date;
 }
 
+/**
+ * Represents a User model.
+ */
 export default (sequelize: Sequelize) => {
+  /**
+   * Represents the User class.
+   */
   User.init(
     {
+      /**
+       * Represents the id of the user.
+       */
       id: {
         type: DataTypes.INTEGER.UNSIGNED,
         autoIncrement: true,
         primaryKey: true,
       },
+      /**
+       * Represents the username of the user.
+       */
       username: {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      /**
+       * Represents the first name of the user.
+       */
       firstName: {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      /**
+       * Represents the last name of the user.
+       */
       lastName: {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      /**
+       * Represents the phone number of the user.
+       */
       phone: {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      /**
+       * Represents the email address of the user.
+       */
       email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
+      /**
+       * Represents the password of the user.
+       */
       password: {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      /**
+       * Represents whether the user is an admin or not.
+       */
       is_admin: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
@@ -57,6 +87,11 @@ export default (sequelize: Sequelize) => {
       tableName: "users",
       modelName: "user",
       hooks: {
+        /**
+         * Hook that is triggered before creating a user.
+         * Hashes the user's password.
+         * @param user - The user object being created.
+         */
         beforeCreate: async (user: any) => {
           user.password = await hashPassword(user.password);
         },
