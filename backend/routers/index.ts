b6f4a58911f1a625795833daaaf75a6721b00082
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-03-30 21:46:47 -0700

    Add routes for user and property APIs

--- backend/routers/index.ts
+++ backend/routers/index.ts
@@ -2,6 +2,11 @@ import { Application } from "express";
 import UserRoutes from "./User.routes";
 import PropertyRoutes from "./Property.routes";
 
+/**
+ * Registers the routes for the application.
+ *
+ * @param {Application} app - The Express application instance.
+ */
 const ROUTES = (app: Application) => {
   app.use("/api/user", UserRoutes);
   app.use("/api/prop", PropertyRoutes);
