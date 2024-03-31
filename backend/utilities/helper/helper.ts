13c4492c65303ddb2bf8d9d9417bce1f7453deb0
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-03-30 23:26:46 -0700

    Add Documentation hashPassword function to helper.ts

--- backend/utilities/helper/helper.ts
+++ backend/utilities/helper/helper.ts
@@ -1,5 +1,10 @@
 import bcryptjs from "bcryptjs";
 
+/**
+ * Hashes a password using bcryptjs.
+ * @param password - The password to be hashed.
+ * @returns A Promise that resolves to the hashed password.
+ */
 export const hashPassword = (password: any): any => {
   const saltRounds = 10;
 
